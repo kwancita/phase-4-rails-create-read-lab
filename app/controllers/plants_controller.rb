1c2973f5073287b091eab3a087ed44257af2a1f3
Wasanta <kwan_u_day@hotmail.com>
2022-01-09 18:46:22 -0500

    commit tree fe5d2a54e1ab178e705f3c1917b412fc9ae6757e

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,33 @@
+class PlantsController < ApplicationController
+  wrap_parameters format: []
+  # GET /birds
+ 
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  # GET /birds/:id
+  def show
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  # POST /birds
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+  # all methods below here are private
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+end
+
+   
